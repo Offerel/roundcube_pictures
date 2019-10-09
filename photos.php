1639560de244798c4c8c555a34ab7a6a5b3fa262
Offerel <Offerel@users.noreply.github.com>
2019-10-09 11:39:00 +0200

    Customizations for Roundcube 1.4 RC2

--- photos.php
+++ photos.php
@@ -2,7 +2,7 @@
 /**
  * Roundcube Pictures Plugin
  *
- * @version 1.4.0
+ * @version 1.4.1
  * @author Offerel
  * @copyright Copyright (c) 2018, Offerel
  * @license GNU General Public License, version 3
