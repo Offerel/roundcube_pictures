e60d91a29f82f4a99dc1dc81fc0a536d6a2c040e
Offerel <offerel@noreply.codeberg.org>
2023-08-09 10:35:47 +0200

    version bump

--- maintain.php
+++ maintain.php
@@ -2,7 +2,7 @@
 /**
  * Roundcube Pictures Plugin
  *
- * @version 1.4.5
+ * @version 1.4.6
  * @author Offerel
  * @copyright Copyright (c) 2023, Offerel
  * @license GNU General Public License, version 3
