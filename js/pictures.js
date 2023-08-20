35bdaf827882d2a89eff108f071ab03f11dd9a53
Offerel <offerel@noreply.codeberg.org>
2023-08-20 21:26:20 +0200

    version

--- js/pictures.js
+++ js/pictures.js
@@ -1,7 +1,7 @@
 /**
  * Roundcube Pictures Plugin
  *
- * @version 1.4.12
+ * @version 1.4.13
  * @author Offerel
  * @copyright Copyright (c) 2023, Offerel
  * @license GNU General Public License, version 3
