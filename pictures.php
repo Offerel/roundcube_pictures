1084fa36bc1d4754e59169f4e7abbe62269f357a
Offerel <offerel@noreply.codeberg.org>
2023-08-21 11:46:54 +0200

    create table for broken images

--- pictures.php
+++ pictures.php
@@ -241,12 +241,10 @@ function checkDB() {
 	$dbh->query($query);
 	$count = $dbh->fetch_array()[0];
 	if($count != 2) {
-		$query = "CREATE TABLE IF NOT EXISTS `pic_pictures` (`pic_id` INTEGER, `pic_path`	TEXT NOT NULL, `pic_type` TEXT NOT NULL, `pic_taken` INTEGER NOT NULL, `pic_EXIF` TEXT, `user_id` INTEGER NOT NULL, PRIMARY KEY(`pic_id` AUTOINCREMENT), UNIQUE(`pic_path`,`user_id`), FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE)";
-		$dbh->query($query);
-		$query = "CREATE TABLE IF NOT EXISTS `pic_shares` (`share_id` INTEGER,`share_name` TEXT NOT NULL,`share_link` TEXT NOT NULL, `expire_date` INTEGER, `user_id` INTEGER NOT NULL, PRIMARY KEY(`share_id` AUTOINCREMENT), FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE)";
-		$dbh->query($query);
-		$query = "CREATE TABLE IF NOT EXISTS `pic_shared_pictures` (`shared_pic_id`	INTEGER, `share_id`	INTEGER NOT NULL, `user_id`	INTEGER, `pic_id` INTEGER, UNIQUE(`share_id`,`pic_id`), PRIMARY KEY(`shared_pic_id` AUTOINCREMENT), FOREIGN KEY(`pic_id`) REFERENCES `pic_pictures`(`pic_id`) ON DELETE CASCADE, FOREIGN KEY(`share_id`) REFERENCES `pic_shares`(`share_id`) ON DELETE CASCADE)";
-		$dbh->query($query);
+		$dbh->query("CREATE TABLE IF NOT EXISTS `pic_pictures` (`pic_id` INTEGER, `pic_path`	TEXT NOT NULL, `pic_type` TEXT NOT NULL, `pic_taken` INTEGER NOT NULL, `pic_EXIF` TEXT, `user_id` INTEGER NOT NULL, PRIMARY KEY(`pic_id` AUTOINCREMENT), UNIQUE(`pic_path`,`user_id`), FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE)");
+		$dbh->query("CREATE TABLE IF NOT EXISTS `pic_shares` (`share_id` INTEGER,`share_name` TEXT NOT NULL,`share_link` TEXT NOT NULL, `expire_date` INTEGER, `user_id` INTEGER NOT NULL, PRIMARY KEY(`share_id` AUTOINCREMENT), FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE)");
+		$dbh->query("CREATE TABLE IF NOT EXISTS `pic_shared_pictures` (`shared_pic_id`	INTEGER, `share_id`	INTEGER NOT NULL, `user_id`	INTEGER, `pic_id` INTEGER, UNIQUE(`share_id`,`pic_id`), PRIMARY KEY(`shared_pic_id` AUTOINCREMENT), FOREIGN KEY(`pic_id`) REFERENCES `pic_pictures`(`pic_id`) ON DELETE CASCADE, FOREIGN KEY(`share_id`) REFERENCES `pic_shares`(`share_id`) ON DELETE CASCADE)");
+		$dbh->query("CREATE TABLE IF NOT EXISTS `pic_broken` (`broken_id`	INTEGER, `user_id`	INTEGER NOT NULL, `pic_path` TEXT NOT NULL, PRIMARY KEY(`broken_id` AUTOINCREMENT), UNIQUE(`pic_path`,`user_id`), FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE)");
 	}
 	$atime = time();
 	$result = $dbh->query("DELETE FROM `pic_shares` WHERE `expire_date` < $atime");
