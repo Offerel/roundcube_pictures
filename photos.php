613f987f3062f144f43fc6784ef547b65f038abc
Offerel <offerel@noreply.codeberg.org>
2023-11-21 09:51:05 +0100

    Request fullscreen gallery

--- photos.php
+++ photos.php
@@ -330,16 +330,21 @@ function showPage($thumbnails, $dir) {
 			},
 			autoplayVideos: false,
 			loop: false,
+			closeOnOutsideClick: false
 		});
 
 		lightbox.on('slide_changed', (data) => {
 			let file = new URL(data.current.slideConfig.href).searchParams.get('file').split('/').slice(-1)[0];
 			if(document.getElementById(file)) {
 				if(document.getElementById('infbtn')) document.getElementById('infbtn').remove();
+				if(document.getElementById('fbtn')) document.getElementById('fbtn').remove();
 				let closebtn = document.querySelector('.gclose');
 				let infobtn = document.createElement('button');
+				let fbtn = document.createElement('button');
 				infobtn.id = 'infbtn';
-				infobtn.innerHTML = '<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.0\" viewBox=\"0 0 160 160\"><g fill=\"white\"><path d=\"M80 15c-35.88 0-65 29.12-65 65s29.12 65 65 65 65-29.12 65-65-29.12-65-65-65zm0 10c30.36 0 55 24.64 55 55s-24.64 55-55 55-55-24.64-55-55 24.64-55 55-55z\"/><path d=\"M89.998 51.25a11.25 11.25 0 1 1-22.5 0 11.25 11.25 0 1 1 22.5 0zm.667 59.71c-.069 2.73 1.211 3.5 4.327 3.82l5.008.1V120H60.927v-5.12l5.503-.1c3.291-.1 4.082-1.38 4.327-3.82V80.147c.035-4.879-6.296-4.113-10.757-3.968v-5.074L90.665 70\"/></g></svg>';
+				fbtn.id = 'fbtn';
+				fbtn.innerHTML = '<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 14 14\"><path fill=\"#fff\" fill-rule=\"evenodd\" d=\"M2 9H0v5h5v-2H2V9ZM0 5h2V2h3V0H0v5Zm12 7H9v2h5V9h-2v3ZM9 0v2h3v3h2V0H9Z\"/></svg>';
+				infobtn.innerHTML = '<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.0\" viewBox=\"0 0 160 160\"><g fill=\"white\"><path d=\"M80 15c-35.88 0-65 29.12-65 65s29.12 65 65 65 65-29.12 65-65-29.12-65-65-65zm0 10c30.36 0 55 24.64 55 55s-24.64 55-55 55-55-24.64-55-55 24.64-55 55-55z\"/><path d=\"M89.998 51.25a11.25 11.25 0 1 1-22.5 0 11.25 11.25 0 1 1 22.5 0zm.667 59.71c-.069 2.73 1.211 3.5 4.327 3.82l5.008.1V120H60.927v-5.12l5.503-.1c3.291-.1 4.082-1.38 4.327-3.82V80.147c.035-4.879-6.296-4.113-10.757-3.968v-5.074L90.665 70\"/></g></svg>';				
 				infobtn.addEventListener('mouseover', function() {
 					document.getElementById(file).classList.add('eshow');
 					document.getElementById(file).addEventListener('mouseover', function() {document.getElementById(file).classList.add('eshow')})
@@ -348,7 +353,16 @@ function showPage($thumbnails, $dir) {
 					document.getElementById(file).classList.remove('eshow');
 					document.getElementById(file).addEventListener('mouseout', function() {document.getElementById(file).classList.remove('eshow')})
 				});
+				fbtn.addEventListener('click', e => {
+					if(document.fullscreenElement){ 
+						document.exitFullscreen() 
+					} else { 
+						document.getElementById('glightbox-body').requestFullscreen();
+					}
+				});
+
 				closebtn.before(infobtn);
+				closebtn.before(fbtn);
 			}
 		});
 
