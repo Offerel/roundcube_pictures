c1f02c1de545f2c75a2c0d924958db570cfa8adc
Offerel <offerel@noreply.codeberg.org>
2024-07-02 10:45:36 +0200

    fix infobtn

--- photos.php
+++ photos.php
@@ -549,17 +549,20 @@ function showPage($thumbnails, $dir) {
 			let closebtn = document.querySelector('.gclose');
 			closebtn.before(dlbtn);
 			closebtn.before(fbtn);
-			
+
+			let iBox = document.getElementById(file);
+			let infobtn = document.createElement('button');
+			infobtn.id = 'infbtn';
+			infobtn.innerHTML = '<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.0\" viewBox=\"0 0 160 160\"><g fill=\"white\"><path d=\"M80 15c-35.88 0-65 29.12-65 65s29.12 65 65 65 65-29.12 65-65-29.12-65-65-65zm0 10c30.36 0 55 24.64 55 55s-24.64 55-55 55-55-24.64-55-55 24.64-55 55-55z\"/><path d=\"M89.998 51.25a11.25 11.25 0 1 1-22.5 0 11.25 11.25 0 1 1 22.5 0zm.667 59.71c-.069 2.73 1.211 3.5 4.327 3.82l5.008.1V120H60.927v-5.12l5.503-.1c3.291-.1 4.082-1.38 4.327-3.82V80.147c.035-4.879-6.296-4.113-10.757-3.968v-5.074L90.665 70\"/></g></svg>';
+			closebtn.before(infobtn);
+
 			if(document.getElementById(file)) {
-				let iBox = document.getElementById(file);
-				let infobtn = document.createElement('button');
-				infobtn.id = 'infbtn';
 				infobtn.dataset.iid = file;
-				infobtn.innerHTML = '<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.0\" viewBox=\"0 0 160 160\"><g fill=\"white\"><path d=\"M80 15c-35.88 0-65 29.12-65 65s29.12 65 65 65 65-29.12 65-65-29.12-65-65-65zm0 10c30.36 0 55 24.64 55 55s-24.64 55-55 55-55-24.64-55-55 24.64-55 55-55z\"/><path d=\"M89.998 51.25a11.25 11.25 0 1 1-22.5 0 11.25 11.25 0 1 1 22.5 0zm.667 59.71c-.069 2.73 1.211 3.5 4.327 3.82l5.008.1V120H60.927v-5.12l5.503-.1c3.291-.1 4.082-1.38 4.327-3.82V80.147c.035-4.879-6.296-4.113-10.757-3.968v-5.074L90.665 70\"/></g></svg>';
 				infobtn.addEventListener('mouseover', iBoxShow, true);
 				infobtn.addEventListener('mouseout', iBoxShow, true);
 				infobtn.addEventListener('click', iBoxShow, true);
-				closebtn.before(infobtn);
+			} else {
+				infobtn.disabled = true;
 			}
 		});
 
