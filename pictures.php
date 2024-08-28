e26d79bd5a05e7ac0d237e5399fb6d7cdd4fc24f
Offerel <offerel@noreply.codeberg.org>
2024-08-28 10:26:37 +0200

    fixed response type

--- pictures.php
+++ pictures.php
@@ -26,6 +26,7 @@ class pictures extends rcube_plugin {
 				'logfile' => $logfile
 			);
 			http_response_code($code);
+			header('Content-Type: application/json');
 			echo json_encode($array);
 			die();
 		}
